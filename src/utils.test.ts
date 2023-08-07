89abb31d33fb255ddcabeab109bbb7e3f9cb3cfa
Ofer Gill <ofer.gill@stateless.net>
2023-08-07 20:44:35 +0000

    Logs Pulumi engine events to an output file

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,14 @@
-import { lock } from "./utils";
+import {
+	lock,
+	getPulumiOutputStream,
+	logEngineEvent,
+	sleep,
+	isPresent,
+} from "./utils";
+import * as path from "path";
+import * as fs from "fs";
+import { ToSynthesize } from "./config";
+import * as pulumi from "@pulumi/pulumi";
 
 describe("deepFreeze", () => {
 	it("should freeze a simple object", () => {
@@ -39,5 +49,112 @@ describe("deepFreeze", () => {
 
 		expect(frozenObj1).toBe(obj1);
 		expect(frozenObj2).toBe(obj2);
+	});
+});
+
+// Tests specific to working directories
+describe("workDir", () => {
+	let workDir = fs.mkdtempSync("utils-test-work-dir");
+	beforeEach(() => {
+		workDir = fs.mkdtempSync("utils-test-work-dir");
+	});
+
+	it("log engine events yield expected pulumi-logs.out", async () => {
+		const args: ToSynthesize = {
+			meshName: "test",
+			projectName: "project",
+			accounts: [],
+			psk: "",
+			workDir,
+			pulumiLogFile: path.join(workDir, "pulumi-logs.out"),
+		};
+
+		const stream = getPulumiOutputStream(args);
+
+		const events: Array<pulumi.automation.EngineEvent> = [
+			{
+				sequence: 0,
+				timestamp: 1000,
+				preludeEvent: {
+					config: { "aws:region": "mordor", "gcp-project": "project-runway" },
+				},
+			},
+			{
+				sequence: 1,
+				timestamp: 1001,
+				diagnosticEvent: {
+					prefix: "debug: ",
+					message: "Testing a pile of nonsense...",
+					color: "never",
+					severity: "info",
+				},
+			},
+			{
+				sequence: 2,
+				timestamp: 1002,
+				resourcePreEvent: {
+					metadata: {
+						op: "same",
+						urn: "urn:pulumi:my-network::my-network::gcp:compute/forwardingRule:ForwardingRule::forwarding-rule/3/ipsec",
+						type: "gcp:compute/forwardingRule:ForwardingRule",
+						provider: "",
+					},
+				},
+			},
+			{
+				sequence: 3,
+				timestamp: 1003,
+				resOutputsEvent: {
+					metadata: {
+						op: "same",
+						urn: "urn:pulumi:my-network::my-network::aws:ec2/vpnGateway:VpnGateway::vpn-gateway/arbitrary-unique-id-aws1/vpc-2",
+						type: "aws:ec2/vpnGateway:VpnGateway",
+						provider:
+							"urn:pulumi:my-network::my-network::pulumi:providers:aws::default_5",
+					},
+				},
+			},
+			{
+				sequence: 4,
+				timestamp: 1004,
+				diagnosticEvent: {
+					prefix: "debug: ",
+					message: "Wait for it...",
+					color: "never",
+					severity: "info",
+				},
+			},
+			{
+				sequence: 5,
+				timestamp: 1005,
+				summaryEvent: {
+					maybeCorrupt: false,
+					durationSeconds: 1000,
+					resourceChanges: { same: 15 },
+					policyPacks: {},
+				},
+			},
+			{ sequence: 6, timestamp: 1006, cancelEvent: {} },
+		];
+		try {
+			for (const event of events) {
+				logEngineEvent(stream, event);
+			}
+		} finally {
+			stream.end();
+
+			// wait for the write stream to be done before reading the file
+			await new Promise((resolve) => stream.on("finish", resolve));
+		}
+
+		const logs = fs.readFileSync(args.pulumiLogFile, "utf-8").split("\n");
+		// parse all nonempty lines as json objects into jsonLogs
+		const jsonLogs = [];
+		for (const log of logs) {
+			if (log.length > 0) {
+				jsonLogs.push(JSON.parse(log));
+			}
+		}
+		expect(jsonLogs).toEqual(events);
 	});
 });
