ea4b93b3b39e28fecacff4d76d5867a3faad28d4
Sarah <sarah.wheeler@stateless.net>
2023-08-30 14:33:37 -0400

    formatting

--- src/discovery/aws.ts
+++ src/discovery/aws.ts
@@ -10,7 +10,6 @@ import { AwsVpc, AwsSubnet, AwsAccount } from "../types/new-types";
 import { isPresent } from "../utils";
 
 export async function getVpcs(account: AwsAccount): Promise<AwsVpc[]> {
-
 	const ec2Client = new EC2Client({
 		region: account.region,
 	});
@@ -22,45 +21,43 @@ export async function getVpcs(account: AwsAccount): Promise<AwsVpc[]> {
 		throw new Error("no regions found!");
 	}
 
-		for (const region of regionRes.Regions) {
-			const ec2Client = new EC2Client({
-				region: region.RegionName,
-			});
+	for (const region of regionRes.Regions) {
+		const ec2Client = new EC2Client({
+			region: region.RegionName,
+		});
 
-			const { Vpcs }: DescribeVpcsCommandOutput = await ec2Client.send(
-				new DescribeVpcsCommand({}),
-			);
+		const { Vpcs }: DescribeVpcsCommandOutput = await ec2Client.send(
+			new DescribeVpcsCommand({}),
+		);
 
-			if (isPresent(Vpcs)) {
-				for (const vpc of Vpcs) {
-					const subnet: DescribeSubnetsCommandOutput = await ec2Client.send(
-						new DescribeSubnetsCommand({
-							Filters: [{ Name: "vpc-id", Values: [vpc.VpcId!] }],
-						}),
-					);
-					const subnets = subnet.Subnets?.map((subnet) =>
-						AwsSubnet.parse({
-							id: subnet.SubnetId,
-							cidr: subnet.CidrBlock,
-							type: "AwsSubnet",
-						}),
-					);
-					vpcs.push(
-						AwsVpc.parse({
-							id: vpc.VpcId,
-							type: "AwsVpc",
-							region: region.RegionName,
-							cidr: vpc.CidrBlock,
-							subnets: subnets,
-						}),
-					);
-				}
+		if (isPresent(Vpcs)) {
+			for (const vpc of Vpcs) {
+				const subnet: DescribeSubnetsCommandOutput = await ec2Client.send(
+					new DescribeSubnetsCommand({
+						Filters: [{ Name: "vpc-id", Values: [vpc.VpcId!] }],
+					}),
+				);
+				const subnets = subnet.Subnets?.map((subnet) =>
+					AwsSubnet.parse({
+						id: subnet.SubnetId,
+						cidr: subnet.CidrBlock,
+						type: "AwsSubnet",
+					}),
+				);
+				vpcs.push(
+					AwsVpc.parse({
+						id: vpc.VpcId,
+						type: "AwsVpc",
+						region: region.RegionName,
+						cidr: vpc.CidrBlock,
+						subnets: subnets,
+					}),
+				);
 			}
+		}
 	}
 
 	return vpcs;
-
 }
 
 export default { getVpcs };
-
